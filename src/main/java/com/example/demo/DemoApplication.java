c2581d823bf74f84b5e010c989bf7b6ae7f2711e
bala-amypo <bala@amypo.in>
2025-12-21 10:12:04 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class demo/src/main/java/com/example/demo/config/SecurityConfig.
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
+demo/src/main/java/com/example/demo/config/SecurityConfig.java
