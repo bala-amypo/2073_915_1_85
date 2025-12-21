ff302ee9b475f4564306deb9c05c156b66c4d160
bala-amypo <bala@amypo.in>
2025-12-21 18:49:22 +0000

    User commit

--- demo/DemoApplication.java
+++ demo/DemoApplication.java
@@ -1,11 +1,11 @@
-package com.example.demo;
+package com.example.demo; // This MUST match the folder path
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-@ServletComponentScan // Important: This allows your @WebServlet to work
+@ServletComponentScan // Mandatory for your @WebServlet(/hello-servlet)
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
