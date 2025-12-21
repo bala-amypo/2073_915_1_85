3523d74144ecc2669cde8eb9a74181c0b2be05f4
bala-amypo <bala@amypo.in>
2025-12-21 16:07:22 +0000

    User commit

---
+++ src/main/java/com/example/demo/SecurtiyConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
