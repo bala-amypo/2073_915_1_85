18facbb8847f3432bc5263f0ba1a543b938b857d
bala-amypo <bala@amypo.in>
2025-12-21 18:04:22 +0000

    User commit

--- src/main/java/com/example/demo/repository/RatingLogRepository.java.java
+++ src/main/java/com/example/demo/repository/RatingLogRepository.java.java
@@ -1,32 +1,6 @@
-package com.example.demo.entity;
+package com.example.demo.repository;
+import com.example.demo.model.RatingLog; // or entity package
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import jakarta.persistence.*;
-import java.time.LocalDateTime;
-
-@Entity
-@Table(name = "rating_logs")
-public class RatingLog {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne
-    private Property property;
-
-    private String message;
-    private LocalDateTime loggedAt;
-
-    public RatingLog() {}
-
-    public RatingLog(Property property, String message, LocalDateTime loggedAt) {
-        this.property = property;
-        this.message = message;
-        this.loggedAt = loggedAt;
-    }
-
-    @PrePersist
-    public void onCreate() {
-        loggedAt = LocalDateTime.now();
-    }
+public interface RatingLogRepository extends JpaRepository<RatingLog, Long> {
 }
