59735e69c681bd70fc4f102b6bc9ae2426166c92
bala-amypo <bala@amypo.in>
2025-12-21 15:46:29 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/FacilityScoreService.java
@@ -0,0 +1,9 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.RatingResult;
+
+public interface RatingService {
+    RatingResult generateRating(Long propertyId);
+    RatingResult getRating(Long propertyId);
+}
+Service
