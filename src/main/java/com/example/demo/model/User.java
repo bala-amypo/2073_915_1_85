abe3f95eb2c75e2f4d0e57ef48b78e9784de226a
bala-amypo <bala@amypo.in>
2025-12-22 05:37:54 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,22 +1,22 @@
-package com.example.demo.entity;
+package com.example.demo.model; // Must match the folder name
 
 import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
 @Table(name = "users")
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
-
     @Column(unique = true, nullable = false)
-    private String email;
+    private String username;
 
+    @Column(nullable = false)
     private String password;
-
-    private String role; // ADMIN / ANALYST
 }
